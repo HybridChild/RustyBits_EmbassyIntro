825641806b1815a2482c3b75ed11861fb6a5f5d5
HybridChild <esbendn@tutanota.com>
2025-05-27 07:22:16 +0200

    Use defmt for logging

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@
 
 use cortex_m_rt::entry;
 use panic_halt as _;
-use rtt_target::{rprintln, rtt_init_print};
-
+use defmt_rtt as _;
+use defmt::info;
 use stm32f0xx_hal::{
     adc::{Adc, VTemp},
     delay::Delay,
@@ -15,34 +15,33 @@ use stm32f0xx_hal::{
 #[entry]
 fn main() -> ! {
     // Initialize RTT
-    rtt_init_print!();
-    rprintln!("Starting program...");
+    info!("Starting program...");
 
     // Get access to cortex-m peripherals
     let cp = cortex_m::peripheral::Peripherals::take().unwrap();
 
     // Get access to the device peripherals
     let mut dp = pac::Peripherals::take().unwrap();
-    rprintln!("Peripherals taken");
+    info!("Peripherals taken");
 
     // Configure the system clock
     // We'll use the default internal oscillator at 8MHz
     let mut rcc = dp.RCC.configure().freeze(&mut dp.FLASH);
-    rprintln!("Clocks configured");
+    info!("Clocks configured");
 
     // Create delay instance
     let mut delay = Delay::new(cp.SYST, &rcc);
 
     // Initialize ADC
     let mut adc = Adc::new(dp.ADC, &mut rcc);
-    rprintln!("ADC initialized");
+    info!("ADC initialized");
 
     // Configure LED pin
     let gpioa = dp.GPIOA.split(&mut rcc);
     let mut user_led = cortex_m::interrupt::free(|cs| {
         gpioa.pa5.into_push_pull_output(cs).downgrade()
     });
-    rprintln!("LED pin configured (PA5: Push-Pull Output)");
+    info!("LED pin configured (PA5: Push-Pull Output)");
 
     let mut blink_counter = 0;
 
@@ -51,13 +50,13 @@ fn main() -> ! {
         // VTemp::read returns temperature in tenths of degrees Celsius
         let temp_tenths = VTemp::read(&mut adc, Some(&mut delay));
         let temp_celsius = temp_tenths as f32 / 10.0;
-        
+
         user_led.toggle().unwrap();
         delay.delay_ms(500_u16);
         user_led.toggle().unwrap();
         delay.delay_ms(500_u16);
         blink_counter += 1;
-        
-        rprintln!("LED blinked {} times, Temperature: {:.1}°C", blink_counter, temp_celsius);
+
+        info!("LED blinked {} times, Temperature: {}°C", blink_counter, temp_celsius);
     }
 }
